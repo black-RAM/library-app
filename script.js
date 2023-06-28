a36ecc80d5b1ddff82a94984c66336c04759c56e
black-RAM <pplanes311@gmail.com>
2023-06-28 11:54:52 +0300

    replaced function Book() with class syntax

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 let library = [];
-
-function Book (title, author, pages, wasRead) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.wasRead = wasRead;
-}
+class Book{
+  constructor(title, author, pages, wasRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.wasRead = wasRead;
+  };
+};
 
 function addBookToLibrary (title, author, pages, wasRead) {
   let book = new Book(title, author, pages, wasRead);
   library.push(book);
 }
+
+// test
+addBookToLibrary("Prince Caspian", "C.S. Lewis", 185, true);
+console.log(library);
